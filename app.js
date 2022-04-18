307326292501789919eb58afa91a8daa8ebd2b70
Leonel Sanchez Usma <2205leosan@gmail.com>
2022-04-18 12:15:21 -0400

    estructura html

--- app.js
+++ app.js
@@ -1,54 +1,27 @@
 const express = require("express");
-
+//const req = require("express/lib/request");
 const app = express();
 
 //manejar ruta de archivos
 const path = require("path");
 
-//usando recursos estaticos
-app.use(express.static('public'));
+const publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
 
 //ponemos a escuchar el servidor
 app.listen(3000, () => {
     console.log("Servidor corriendo en http://localhost:3000")
 });
 
-//Definimos las rutas a los distintos pedidos que nuestro sitio sabe responder
-
-
-app.get("/compra", (req, res)=> {
-    res.send("Compra Exitosa")
-})
-
-app.get("/contacto", (req, res)=> {
-    res.send("Dejanos tu contacto")
-})
-
-app.get("/un_array", (req, res)=> {
-    res.send([1, 2, 3, 4])
-})
-
-app.get("/un_objeto", (req, res)=> {
-    res.send({
-        name: "Leonel",
-        apellido: 'Sanchez'
-    })
-})
-
-let producto = {
-    tipoProducto: "null",
-    precio:"null", 
-    cantidad:"null"
-}
-
-app.get("/producto", (req, res)=>{
-    res.send(producto)
-})
 
 app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/index.html"));
+   
     //let htmlPath = path.resolve(__dirname, "./views/index.html");
     //res.sendFile(htmlPath)
-    res.sendFile(path.join(__dirname, "./views/index.html"))     //otra forma de escribir el codigo
+    //res.sendFile(path.join(__dirname, "./views/index.html"))     //otra forma de escribir el codigo
     
-})
+});
+
+
     
