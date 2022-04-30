68d7e312703bfcc71f8d5ae34c689a1c608e4c78
Leonel Sanchez Usma <2205leosan@gmail.com>
2022-04-30 18:30:08 -0400

    login css

--- app.js
+++ app.js
@@ -5,23 +5,26 @@ const app = express();
 //manejar ruta de archivos
 const path = require("path");
 
-const publicPath = path.resolve(__dirname, "public");
+const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 //ponemos a escuchar el servidor
-app.listen(3000, () => {
-    console.log("Servidor corriendo en http://localhost:3000")
-});
+app.listen(process.env.PORT || 3000, () => console.log("Servidor corriendo en el puerto http://localhost:3000"));
 
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/index.html"));
+})
+
+app.route("/*").get(function (req, res) {
+    return res.sendFile(path.join(__dirname, "./views/login.html"));
+});
    
     //let htmlPath = path.resolve(__dirname, "./views/index.html");
     //res.sendFile(htmlPath)
     //res.sendFile(path.join(__dirname, "./views/index.html"))     //otra forma de escribir el codigo
     
-});
+
 
 
     
